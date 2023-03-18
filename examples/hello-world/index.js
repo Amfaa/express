a0f7b796dd8650d456aa04eb11780566290de7f6
Syed Mazhar Ali <87525103+Amfaa@users.noreply.github.com>
2023-03-18 23:45:09 +0530

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello World PearlThoughts');
+  res.send('Hello World India');
 });
 
 /* istanbul ignore next */
